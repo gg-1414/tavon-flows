241ad6d3c76ced0a7a3a6b08c90f49a87ba17cd6
Gina Lee <gina@thesistesting.com>
2021-02-12 00:29:30 -0800

    successful nav & page transition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { PageTransition } from "@steveeeie/react-page-transition";
+import './App.scss';
+import Nav from './components/Nav.js';
+import Home from './pages/Home/Home.js';
+import About from './pages/About/About.js';
+import Work from './pages/Work/Work.js';
+import Gallery from './pages/Gallery/Gallery.js';
+import Contact from './pages/Contact/Contact.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Route
+        render={({ location }) => {
+          return (
+            <PageTransition
+              preset="moveToLeftFromRight"
+              transitionKey={location.pathname}
+            >
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/work" component={Work} />
+                <Route exact path="/gallery" component={Gallery} />
+                <Route exact path="/contact" component={Contact} />
+              </Switch>
+            </PageTransition>
+          );
+        }}
+      />
+    </BrowserRouter>
   );
 }
 
